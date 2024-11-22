be10dcef118b1293f3dd22acc3f59d6583ddb885
ben05allen <ben05allen@hotmail.com>
2024-11-22 22:32:05 +0900

    starting on generator

--- app/create_invoice.py
+++ app/create_invoice.py
@@ -6,13 +6,53 @@ from invoicees import get_invoicees
 
 
 BASE_PATH = Path(__file__).parents[1]
-with open(BASE_PATH / "pyproject.toml", "rb") as f:
-    config = tomllib.load(f).get("tool", {}).get("genvoice", {})
 
-bank_instructions_path = Path(config["bank_instructions_path"])
-INSTRUCTIONS = get_instructions(bank_instructions_path)
 
-invoicees_path = Path(config["invoicees_path"])
-INVOICEES = get_invoicees(invoicees_path)
+class Details:
+    def __init__(
+        self,
+        base_path: str | Path,
+        invoicees_path: str | Path | None = None,
+        bank_instructions_path: str | Path | None = None,
+    ):
+        if isinstance(base_path, str):
+            base_path = Path(base_path)
+        self.base_path = base_path
+
+        if isinstance(invoicees_path, str):
+            invoicees_path = base_path / invoicees_path
+        self.invoicees_path = invoicees_path
+
+        if isinstance(bank_instructions_path, str):
+            bank_instructions_path = base_path / bank_instructions_path
+        self.bank_instructions_path = bank_instructions_path
+
+    @classmethod
+    def load_tool_config(cls, base_path: str | Path):
+        if isinstance(base_path, str):
+            base_path = Path(base_path)
+
+        with open(base_path / "pyproject.toml", "rb") as f:
+            config = tomllib.load(f).get("tool", {}).get("genvoice", {})
+
+        bank_instructions_path = config["bank_instructions_path"]
+        invoicees_path = config["invoicees_path"]
+
+        return cls(
+            base_path,
+            invoicees_path=invoicees_path,
+            bank_instructions_path=bank_instructions_path,
+        )
+
 
 # TODO: Generate JSON for customer invoice
+def main():
+    details = Details.load_tool_config(BASE_PATH)
+
+    instructions = get_instructions(details.bank_instructions_path)
+
+    invoicees = get_invoicees(details.invoicees_path)
+
+
+if __name__ == "__main__":
+    main()
