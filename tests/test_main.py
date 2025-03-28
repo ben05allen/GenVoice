a0909df03f9391bd0c6f5137cd1d387f198a25f9
ben05allen <ben05allen@hotmail.com>
2025-03-28 18:19:29 +0900

    adding freezegun, test for main

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,16 +3,49 @@
 
 import pytest
 from unittest.mock import patch
-from tempfile import NamedTemporaryFile, TemporaryDirectory
+from freezegun import freeze_time
 
 from genvoice import main
+import genvoice.processor
 
 
-def test_main_success():
+@pytest.fixture()
+def test_template(tmp_path):
+    template_text = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Minimal Jinja2 Test</title>
+</head>
+<body>
+  <h1>Hello, Jinja2!</h1>
+  <p>This is a minimal template for testing purposes.</p>
+  <p>{{ test_string }}</p>
+</body>
+</html>
+        """
+
+    template_file = tmp_path / "test.html"
+    template_file.write_text(template_text)
+
+    yield str(template_file)
+
+
+def mock_get_template_data(i: int):
+    return {"test_string": f"invoice {i}"}
+
+
+def test_main_success(test_template, tmp_path, monkeypatch):
+    template_file = test_template
+    dest_folder = tmp_path / "dest"
+    monkeypatch.setattr(genvoice.processor, "get_template_data", mock_get_template_data)
+
     # Simulate command-line arguments
-    test_args = (
-        "prog --invoice 1 --template path/to/tplt --destination path/to/dest".split()
-    )
+    test_args = f"prog -i 1 -t {template_file} -d {dest_folder}".split()
 
-    with patch("sys.argv", test_args):
-        pass
+    with patch("sys.argv", test_args), freeze_time("2020-01-01 12:00:00"):
+        main()
+
+        assert mock_get_template_data(1) == {"test_string": "invoice 1"}
+        assert (dest_folder / "invoice_1_20200101120000.pdf").exists()
